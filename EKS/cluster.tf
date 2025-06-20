491e384b4d36ea425bdf151f069b7c2874f874ca
LAREDO LANCER <lancerajayi@yahoo.com>
2025-06-20 23:31:50 +0100

    Revalidate Codes 32

--- EKS/cluster.tf
+++ EKS/cluster.tf
@@ -31,7 +31,7 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      instance_types   = ["t3.medium"]
+      instance_types   = ["t2.micro"]
       desired_capacity = 2
       min_size         = 1
       max_size         = 4
